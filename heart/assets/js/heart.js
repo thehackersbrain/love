ee0a035190ebe377aaa1c862bc629a7bf5fb9a74
Gaurav Raj <techw803@gmail.com>
2022-02-16 02:32:33 +0530

    Added heart folder

---
+++ heart/assets/js/heart.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".container");
+for (var i = 1; i <= 5; i++) {
+  const hearts = document.createElement("div");
+  hearts.classList.add("heart");
+  container.appendChild(hearts);
+}
+
+function animateHearts() {
+  anime({
+    targets: ".hearts",
+    translateX: function () {
+      return anime.random(-700, 700);
+    },
+    translateY: function () {
+      return anime.random(-500, 500);
+    },
+    rotate: 45,
+    scale: function () {
+      return anime.random(1, 5);
+    },
+    easing: "easeInOutBack",
+    duration: 3000,
+    delay: anime.stagger(10),
+  });
+}
+
+animateHearts();
